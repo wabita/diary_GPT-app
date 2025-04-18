f8ab6263776f199fd0f323f98b70b455816cafc5
yokoyama asa <sayosayo30g@gmail.com>
2025-04-18 14:59:42 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel,Field
+from datetime import date
+
+app = FastAPI()
+
+class DiaryInput(BaseModel):
+    title: str
+    content: str
+
+class DiaryOutput(DiaryInput):
+    posted_on: date
+
+@app.get("/")
+def home():
+    return {"message": "このサイトは日記投稿APIです。"}
+    
+
+@app.post("/items/", response_model = DiaryOutput)
+def create_item(item: DiaryInput):
+    today =date.today()
+    return DiaryOutput(title=item.title, content=item.content, posted_on=today)
