44ba35020208c8353a88134928002f1059711b7b
yokoyama asa <sayosayo30g@gmail.com>
2025-04-19 18:51:50 +0900

    json save

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel,Field
 from datetime import date
+import json
+import os
 
 app = FastAPI()
 
@@ -15,15 +17,25 @@ class DiaryOutput(DiaryInput):
 def home():
     return {"message": "このサイトは日記投稿APIです。"}
     
-diary_list =[]
+filename = "diary.json"
+diary_data=[]
+
+if os.path.exists(filename):
+    with open(filename,"r",encoding="utf-8") as f:
+        diary_data =json.load(f)
+
 
 @app.post("/items/", response_model = DiaryOutput)
 def create_item(item: DiaryInput):
     today =date.today()
     diary= DiaryOutput(title=item.title, content=item.content, posted_on=today)
-    diary_list.append(diary)
+    diary_data.append(diary.model_dump())
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(diary_data, f, ensure_ascii=False)
     return diary
 
-@app.get("/diary_page")
+@app.get("/diaries/")
 def get_diary():
-    return diary_list
+    with open(filename,"r",encoding="utf-8") as f:
+        data= json.load(f)
+    return data
